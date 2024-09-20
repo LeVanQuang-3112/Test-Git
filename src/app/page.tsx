b5091292bd41260db30a0f4c7fc820306ad8d0ac
nhatpq-rk <nhatpq@rikkeisoft.com>
2024-09-20 09:37:48 +0700

    feat: test git

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 export default function HomePage() {
   return (
     <div className="p-3">
-      <h1>Home page</h1>
       <h1>Home rebase</h1>
       <h1>Home test git</h1>
       <Link href={"/feed"} className="mx-2">
